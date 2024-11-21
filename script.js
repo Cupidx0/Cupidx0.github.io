1055526f9d9760584f71af4be43e96552792a486
Cupidx0 <107266946+Cupidx0@users.noreply.github.com>
2024-11-21 08:36:19 +0000

    updates main

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const skillElements = document.querySelector("skill");
-    
-    skillElements.forEach((skill) => {
-        skill.addEventListener('touchstart',() =>{
-            skill.classList.add('hovered');
+document.addEventListener("DOMContentLoaded",function(){
+    const skills ={
+        html:40,
+        css:30,
+        js:10,
+        python:50
+    };
+    function updateSkills(){
+        for(const skill in skills){
+            const skillElement = document.getElementById(skill);
+            if(skillElement){
+                skillElement.textContent = skills[skill] + '%';
+                skillElement.style.width = skills[skill] + '%';
+                skillElement.dataset.skillValue = skills[skill];
+            }
+        }
+    }
 
-        });
-        skill.addEventListener('touchend',()=>{
-            skill.classList.remove('hovered');
-        });
+    function animateSkills() {
+        for(const skill in skills){
+            const skillStyle = document.getElementById(skill);
+            if(skillStyle){
+                skillStyle.style.width = '0'; 
+                skillStyle.addEventListener('mouseenter', function() {
+                    const skillValue = this.dataset.skillValue;
+                    this.style.width = skillValue + '%';
+                });
+                skillStyle.addEventListener('mouseleave', function() {
+                    this.style.width = '0';
+                });
+            }
+        }
+    }
 
-        skill.addEventListener('mouseenter', () => {
-            skill.classList.add('hovered');
-        });
-            
-        skill.addEventListener('mouseleave', () => {
-            skill.classList.remove('hovered');
-        });
-    });
+    updateSkills();
+    animateSkills();
 });
